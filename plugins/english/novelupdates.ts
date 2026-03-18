62b41f2389dad875a54278d9596443e7d04bd58d
Lukas <lukas.richter.berlin@web.de>
2026-03-18 19:22:05 +0100

    revert versions

--- plugins/english/novelupdates.ts
+++ plugins/english/novelupdates.ts
@@ -6,7 +6,7 @@ import { Plugin } from '@/types/plugin';
 class NovelUpdates implements Plugin.PluginBase {
   id = 'novelupdates';
   name = 'Novel Updates';
-  version = '0.9.13';
+  version = '0.9.7';
   icon = 'src/en/novelupdates/icon.png';
   customCSS = 'src/en/novelupdates/customCSS.css';
   site = 'https://www.novelupdates.com/';
